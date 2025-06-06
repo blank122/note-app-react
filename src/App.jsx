a5dae7875d2ddd904f1a146376aedb36f0444074
Joshua De Vera <deverajoshua02@gmail.com>
2025-06-06 09:50:17 -0500

    installed shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,10 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <h1 className='text-3xl font-bold underline'>Hello react jsx</h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="underline">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
